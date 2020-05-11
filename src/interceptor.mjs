ffc35305c0d1d4bd773ae51637191ebe7f964868
Markus Felten <Markus.Felten@gmx.de>
2020-05-11 14:58:08 +0200

    fix: make use of StreamTransformInterceptor

--- src/interceptor.mjs
+++ src/interceptor.mjs
@@ -1,18 +1,18 @@
 import LineParser from "./line-parser.mjs";
-import { Interceptor } from "@kronos-integration/interceptor";
+import { StreamTransformInterceptor } from "@kronos-integration/interceptor";
 
 /**
  * This interceptor cares about the handling of the messages.
  * It will add the hops and copies the messages
  */
-export class LineParserInterceptor extends Interceptor {
+export class LineParserInterceptor extends StreamTransformInterceptor {
   static get name() {
     return "line-parser";
   }
 
-  async receive(endpoint, next, request) {
-    const streamFilter = new LineParser(this, true);
-    return next(request.pipe(streamFilter));
+  createTransformer()
+  {
+    return new LineParser(this, true);
   }
 }
 
